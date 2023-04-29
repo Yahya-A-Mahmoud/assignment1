152da0992e25a932ff32cbc828faf78d9e29f817
Yahya-A-Mahmoud <132074250+Yahya-A-Mahmoud@users.noreply.github.com>
2023-04-29 14:54:56 +0200

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ### In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# + List popular languages for Data Science
+# + List popular libraries for Data Science
+# + Data Science tools
+# + Convert 200 minutes to hours
+
+# 1. Python
+# 2. R
+# 3. Julia
+# 4. Scala
+# 5. SQL
+# 6. MATLAB
+
+# 1. NumPy: A library for numerical computing in Python.
+# 2. Pandas: A library for data manipulation and analysis.
+# 3. Matplotlib: A library for creating visualizations in Python.
+# 4. Scikit-learn: A library for machine learning algorithms and tools.
+# 5. TensorFlow: A library for building and training machine learning models.
+# 6. Keras: A high-level API for building and training deep learning models.
+# 7. PyTorch: A library for building and training machine learning models, particularly for deep learning.
+# 8. Seaborn: A library for creating statistical visualizations in Python.
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyter Notebook |
+# | PyCharm |
+# | VS Code |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[6]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[7]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ## Author
+# Yahya
+
+# In[ ]:
+
+
+
+
